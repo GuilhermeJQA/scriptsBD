f8c26e917fc169a41acbe8f9c24f4e9d2a28845b
Guilherme José <101995065+GuilhermeJQA@users.noreply.github.com>
2022-12-02 18:52:36 -0300

    Add files via upload

---
+++ SelectPerguntas.sql
@@ -0,0 +1,14 @@
+-- 1 - Quais clientes tem mais de 20 anos?
+    SELECT *
+    FROM DBO.CLIENTES 
+    WHERE IDADE > 20
+   
+-- 2 - Quais clientes tem menos de 20 anos?
+    SELECT *
+    FROM DBO.CLIENTES 
+    WHERE IDADE < 20
+    
+-- 3 - Qual clientes tem idade igual a 23 anos?
+    SELECT *
+    FROM DBO.CLIENTES 
+    WHERE IDADE = 20
